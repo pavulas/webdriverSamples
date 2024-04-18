67ac23e5e4cacc1871dae8389ba14b1c8ca571fb
Administrator <Administrator@172.31.5.73>
2024-04-18 07:16:05 +0000

    Older Chrome Version

--- src/test/java/testScripts/GooglePageTest.java
+++ src/test/java/testScripts/GooglePageTest.java
@@ -2,11 +2,14 @@ package testScripts;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GooglePageTest {
 
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.setBrowserVersion("115");
+		WebDriver driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 		System.out.println("Page title :  " + driver.getTitle());
