b492cd022c54f3e01bbbc1e6a2a1c09131ac422b
Administrator <Administrator@172.31.5.73>
2024-04-18 07:51:35 +0000

    Web Element by ID

--- src/test/java/testScripts/GooglePageTest.java
+++ src/test/java/testScripts/GooglePageTest.java
@@ -1,19 +1,28 @@
 package testScripts;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GooglePageTest {
 
 	public static void main(String[] args) {
-		ChromeOptions options = new ChromeOptions();
-		options.setBrowserVersion("115");
-		WebDriver driver = new ChromeDriver(options);
+		//ChromeOptions options = new ChromeOptions();
+		//options.setBrowserVersion("115");
+		//WebDriver driver = new ChromeDriver(options);
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 		System.out.println("Page title :  " + driver.getTitle());
-		// TODO Auto-generated method stub
+		WebElement searchbox = driver.findElement(By.id("APjFqb"));
+		searchbox.sendKeys("Pranitha Avula");
+		searchbox.sendKeys(Keys.ENTER);
+		//WebElement namepage = driver.findElement(By.className("LC20lb MBeuO DKV0Md"));
+		//searchbox.sendKeys(Keys.ENTER);
+		System.out.println("Page title :  " + driver.getTitle());
 
 	}
 
