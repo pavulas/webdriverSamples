068c310fd474703158524cea03a910627ff2cb1d
Administrator <Administrator@172.31.5.59>
2024-04-22 06:40:17 +0000

    New Class Added

--- src/test/java/testScripts/GooglePageTest.java
+++ src/test/java/testScripts/GooglePageTest.java
@@ -10,19 +10,23 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class GooglePageTest {
 
 	public static void main(String[] args) {
-		//ChromeOptions options = new ChromeOptions();
-		//options.setBrowserVersion("115");
-		//WebDriver driver = new ChromeDriver(options);
+//		ChromeOptions options = new ChromeOptions();
+//		options.setBrowserVersion("115");
+//		WebDriver driver = new ChromeDriver(options);
+//		options.addArguments("--headless");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.google.com/");
+//		driver.get("https://www.google.com/");
+		driver.navigate().to("https://www.google.com/");
 		System.out.println("Page title :  " + driver.getTitle());
 		WebElement searchbox = driver.findElement(By.id("APjFqb"));
 		searchbox.sendKeys("Pranitha Avula");
 		searchbox.sendKeys(Keys.ENTER);
-		//WebElement namepage = driver.findElement(By.className("LC20lb MBeuO DKV0Md"));
-		//searchbox.sendKeys(Keys.ENTER);
+		driver.findElement(By.xpath("//h3[@class='LC20lb MBeuO DKV0Md']")).click();
 		System.out.println("Page title :  " + driver.getTitle());
+//		driver.navigate().back();
+//		driver.navigate().forward();
+//		driver.navigate().refresh();
 
 	}
 
---
+++ src/test/java/testScripts/InternetLoginPageTest.java
@@ -0,0 +1,25 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InternetLoginPageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://the-internet.herokuapp.com/login");
+//		driver.findElement(By.id("username")).sendKeys("tomsmith");
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("tomsmith");
+		driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+//		driver.findElement(By.className("radius")).click();
+//		driver.findElement(By.linkText("Elemental Selenium")).click();
+//		driver.findElement(By.partialLinkText("Elemental")).click();
+		driver.findElement(By.xpath("//i[@class='fa fa-2x fa-sign-in']")).click();
+		driver.findElement(By.xpath("//i[@class='icon-2x icon-signout']")).click();
+
+	}
+
+}
